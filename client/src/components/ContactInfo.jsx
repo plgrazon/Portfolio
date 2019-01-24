eb771f9a9e472aa5db4edc9a9ab6e59f3a4f2371
Paolo Razon <plgrazon@gmail.com>
2019-01-23 20:57:34 -0800

    [app] - Fix margin on mobile view and update contact links.

--- client/src/components/ContactInfo.jsx
+++ client/src/components/ContactInfo.jsx
@@ -8,20 +8,28 @@ const ContactInfo = () => {
   return (
     <section className={style.contactInfo} id="contactInfo">
       <h1>Get in touch</h1>
-      <div>
-        <a href="https://paolorazon.io">
+      <div className={style.contactLinks}>
+        <a
+          className={style.contactIcon}
+          href="mailto:plgrazon@gmail.com"
+          target="_blank"
+        >
           <FontAwesomeIcon icon="envelope" size="lg" />
         </a>
         {/* <p> plgrazon@gmail.com</p> */}
-      </div>
-      <div>
-        <a href="https://github.com/plgrazon">
+        <a
+          className={style.contactIcon}
+          href="https://github.com/plgrazon"
+          target="_blank"
+        >
           <FontAwesomeIcon icon={['fab', 'github']} size="lg" />
         </a>
         {/* <p> github.com/plgrazon</p> */}
-      </div>
-      <div>
-        <a href="https://www.linkedin.com/in/paolo-razon">
+        <a
+          className={style.contactIcon}
+          href="https://www.linkedin.com/in/paolo-razon"
+          target="_blank"
+        >
           <FontAwesomeIcon icon={['fab', 'linkedin']} size="lg" />
         </a>
         {/* <p> linkedin.com/in/paolo-razon</p> */}
